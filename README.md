687408f8e93c34a67404385229f9ed8e1bbef32f
shruucchan <shrudc@gmail.com>
2016-07-21 23:15:04 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# terebi
-A simple library to handle pixel perfect scaling of window content in Love2D.
+terebi
+======
+
+A simple library to handle pixel-perfect scaling of window content in Love2D.
+
+example
+-------
+
+    local Terebi = require 'terebi'
+
+    function love.load(arg)
+      input = initializeSomeDopeAsHeckInputLibrary()
+
+      -- Set nearest-neighbour scaling, disables mouse.
+      Terebi.initializeLoveDefaults()
+
+      -- Parameters: game width, game height, starting scale factor
+      screen = Terebi.newScreen(320, 240, 2)
+    end
+
+    function love.update(dt)
+      if input:pressed('i') then
+        screen:increaseScale()
+      end
+      if input:pressed('d') then
+        screen:decreaseScale()
+      end
+      if input:pressed('f') then
+        screen:toggleFullscreen()
+      end
+    end
+
+    function love.draw()
+      lg.setCanvas(screen:getCanvas())
+
+      -- Your drawing logic starts here.
+      lg.setColor(150, 181 , 218)
+      lg.rectangle('fill', 50, 50, 50, 50)
+      -- Your drawing logic ends here.
+
+      screen:draw()
+    end
---
+++ terebi.lua
@@ -0,0 +1,112 @@
+local Terebi = {}
+
+
+
+function Terebi.initializeLoveDefaults()
+  love.mouse.setVisible(false)
+
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  love.graphics.setLineStyle('rough')
+end
+
+
+
+local Screen = {}
+local ScreenMetaTable = {__index = Screen}
+
+function Terebi.newScreen(...)
+  return setmetatable({}, ScreenMetaTable)
+    :initialize(...)
+end
+
+function Screen:initialize(width, height, scale)
+  self.width = width
+  self.height = height
+  self.canvas = love.graphics.newCanvas(width, height)
+
+  return self
+    :setScale(scale)
+end
+
+function Screen:getCanvas()
+  return self.canvas
+end
+
+function Screen:setScale(scale)
+  local desktopW, desktopH = love.window.getDesktopDimensions()
+  if scale <= 0 or (self.width * scale > desktopW) or (self.height * scale > desktopH) then
+    return
+  end
+
+  self.scale = scale
+
+  self:updateDrawOffset()
+
+  love.window.setMode(self.width * scale, self.height * scale)
+
+  return self
+end
+
+function Screen:updateDrawOffset()
+  if love.window.getFullscreen() then
+    -- When fullscreen, center screen on monitor
+    local desktopW, desktopH = love.window.getDesktopDimensions()
+    local scaledWidth  = self.width * self.scale
+    local scaledHeight = self.height * self.scale
+    self.drawOffsetX = math.floor((desktopW - scaledWidth) / 2)
+    self.drawOffsetY = math.floor((desktopH - scaledHeight) / 2)
+
+  else
+    self.drawOffsetX = 0
+    self.drawOffsetY = 0
+  end
+end
+
+function Screen:increaseScale()
+  self:setScale(self.scale + 1)
+
+  return self
+end
+
+function Screen:decreaseScale()
+  self:setScale(self.scale - 1)
+
+  return self
+end
+
+function Screen:toggleFullscreen()
+  if love.window.getFullscreen() then
+    love.window.setFullscreen(false)
+    self:setScale(2)
+
+  else
+    self:setMaxScale()
+    love.window.setFullscreen(true)
+  end
+end
+
+function Screen:setMaxScale()
+  local desktopW, desktopH = love.window.getDesktopDimensions()
+  local maxScaleX = math.floor(desktopW / self.width)
+  local maxScaleY = math.floor(desktopH / self.height)
+  local scale = math.min(maxScaleX, maxScaleY)
+  self:setScale(scale)
+end
+
+function Screen:draw()
+  love.graphics.setCanvas()
+  love.graphics.setColor(255, 255, 255, 255)
+
+  local x, y = self:getDrawOffset()
+  love.graphics.draw(self.canvas, self.drawOffsetX, self.drawOffsetY, 0, self.scale, self.scale)
+
+  return self
+end
+
+function Screen:getDrawOffset()
+  return 0, 0
+end
+
+
+
+return Terebi
